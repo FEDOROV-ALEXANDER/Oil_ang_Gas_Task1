38c98e751f7c9ce3c25f6b3f81aa1e3751252481
FEDOROV <fedorov.alexander.04@gmail.com>
2024-11-05 19:04:20 +0300

    На данный момент реализован явный метод.

--- Нефтегаз Ресчетка/Solve.py
+++ Нефтегаз Ресчетка/Solve.py
@@ -1,16 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def well_boundary_condition(X, Y, p, q, coef, N_x, N_y, r_w):
-    for i in range(N_x - 1):
-        for j in range(N_y - 1):
-            r = np.sqrt((X[i] - X[-1] / 2) ** 2 + (Y[j] - Y[-1] / 2) ** 2)
+    for i in range(N_x):
+        for j in range(N_y):
+            r = np.sqrt((X[i]) ** 2 + (Y[j]) ** 2)
             if r <= r_w:
-                p[i, j] = 100 # q * coef * r_w
-                print(p[i, j], q, coef, r_w)
+                p[i, j] +=  10 #q * coef * r_w
     return p
-
 
 def solve_for_one_well(X, Y, x_w, y_w, q, r_w, coef, pressure_start, T, dt, eta):
     X = X - x_w
@@ -18,17 +15,20 @@ def solve_for_one_well(X, Y, x_w, y_w, q, r_w, coef, pressure_start, T, dt, eta)
     dx = X[1] - X[0]
     dy = Y[1] - Y[0]
     N_x, N_y = X.shape[0], Y.shape[0]
-    well_boundary_condition(X, Y, pressure_start, q, coef, N_x, N_y, r_w)
 
-    for t in range(1, T + dt, dt):
+    for t in range(1, int(T / dt) + 1):
         p_new = pressure_start.copy()
-        for i in range(N_x - 1):
-            for j in range(N_y - 1):
+        for i in range(1, N_x - 1):
+            for j in range(1, N_y - 1):
                 p_new[i, j] = pressure_start[i, j] + eta * dt * (
                         (pressure_start[i + 1, j] - 2 * pressure_start[i, j] + pressure_start[i - 1, j]) / dx ** 2 +
                         (pressure_start[i, j + 1] - 2 * pressure_start[i, j] + pressure_start[i, j - 1]) / dy ** 2
                 )
+        pressure_start = well_boundary_condition(X, Y, p_new, q, coef, N_x, N_y, r_w)
 
-        well_boundary_condition(X, Y, p_new, q, coef, N_x, N_y, r_w)
-        pressure_start = p_new
-    return pressure_start, pressure_start[np.where(X == x_w), np.where(Y == y_w)]
+    # Найдем давление в точке скважины
+    well_index_x = np.argmin(np.abs(X))
+    well_index_y = np.argmin(np.abs(Y))
+    pressure_w = pressure_start[well_index_x, well_index_y]
+
+    return pressure_start, pressure_w
