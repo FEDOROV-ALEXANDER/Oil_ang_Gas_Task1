b8d77341b121fe3b29f6c565de48c7b694d6cb3c
FEDOROV <fedorov.alexander.04@gmail.com>
2024-11-05 18:20:02 +0300

    Реализовал расчет давления, но есть ошибка, где то я обнуляю все давление

--- Нефтегаз Ресчетка/main.py
+++ Нефтегаз Ресчетка/main.py
@@ -1,7 +1,8 @@
 import numpy as np
+from Solve import solve_for_one_well
 import matplotlib.pyplot as plt
 import seaborn as sns
-from IPython.core.pylabtools import figsize
+
 
 # Ввод входных значений
 length, width = 15000, 15000  # [м] геометрические размеры рассчитываемой области
@@ -9,17 +10,18 @@ dx, dy = 100, 100  # [м] шаг по направлениям
 Nx, Ny = int(length / dx) + 1, int(width / dy) + 1 # количество элементов
 X = np.linspace(0, length, Nx)
 Y = np.linspace(0, width, Ny)
-x, y = np.meshgrid(X, Y)
+T = 365
+
 
 # координаты скважины
 x_w = 3000
 y_w = 3000
-i_w  =int(x_w / dx) + 1
-j_w = int(y_w / dy) + 1
+r_w = 1.5
 # пока будет одна скважина, нагнетательная
 q_injection = 10  # [м3/сут] дебит нагнетательных скважин
 
-
+B = 5  # Объемный коэффициент
+h = 20  # толщина пласта
 
 
 # TODO надо разобраться с тензором проницаемости. Пока задаю одинаковым во всех направлениях для каждого объема
@@ -31,38 +33,27 @@ permeability_field = np.full((Nx, Ny), permeability)  # проницаемост
 # параметры взяты плюс-минус от балды
 viscosity = 1  # Вязкость [сПз] = 10e-3 [Па * с]
 compressibility = 5 * 10e-9  # [1/Па] сжимаемость
-
+coef = - B * viscosity/ 2 / np.pi / permeability / h # вспомогательный коэффициент
 dt = 1  # шаг по времени в сутках
 
+eta = 1.0
+pressure_start = np.full((Nx, Ny), 0)
+pressure_start[0, :] = 0
+pressure_start[-1, :] = 0
+pressure_start[:, 0] = 0
+pressure_start[:, -1] = 0
+
+pressure, pressure_w = solve_for_one_well(X, Y, x_w, y_w,q_injection, r_w, coef, pressure_start.copy(), T, dt, eta)
 # Начальные и граничные условия: давление нулевое, пласт бесконечный -> давление на границах 0
 # TODO разобраться, какое значение брать у потенциала Ф, на 117 странице Лэйка1_B он взят постоянным, но хз насколько так можно, потому что он определяется как разность давления жидкости и градиента давления под действием силы тяжести
-
 # Сначала сделаю явную схему интегрирования, потому что как то не доходит прикол решения методом прогонки: единственное что смог найти это про построение 5 диагональной матрицы
-def solve_for_one_well(x, y, x_w, y_w, q, p_field_initial):
-    B = 5 # Объемный коэффициент
-    h = 20 # толщина пласта
-    # преобразование координат так, чтобы в начале координат была скважина
-    x_shifted = x - x_w
-    y_shifted = y - y_w
-    r_values = np.sqrt(x_shifted ** 2 + y_shifted ** 2)
-    theta_values = np.arctan2(y_shifted, x_shifted)
-
-    # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    # ax.scatter(theta_values, r_values, color='blue')
-    # ax.set_title('Сетка в полярной системе координат')
-    # plt.show()
-
-    # начальные условия
-    p_field = p_field_initial
-    # граничные условия на внешней границе
-    p_field[:, 0 ] = 0
-    p_field[:, -1] = 0
-    p_field[0, :] = 0
-    p_field[-1, :] = 0
-    # Граничные условия на забое скважины
-    p_field[i_w, j_w] = - q * B * viscosity/ 2 / np.pi / permeability / h
-    #TODO понять, как матрицу давлений перенести в полярную ск
 
 
-
-solve_for_one_well(x, y, x_w, y_w, q_injection, p_field_initial=np.zeros((Nx, Ny)))
+# Визуализация распределения давления
+plt.figure(figsize=(8, 6))
+plt.pcolormesh(X, Y, pressure, shading='auto', cmap='viridis')
+plt.colorbar(label='Давление')
+plt.title(f'Распределение давления через {T} дней')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
