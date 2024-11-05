ed77b16a9967622bb569e8447ed5e4313483e529
FEDOROV <fedorov.alexander.04@gmail.com>
2024-11-05 16:02:27 +0300

    отказался от масштабирования сетки, встроенными методами это не сделать так, как я хочу. Реализовал преобразование координат и часть вычислений

--- Нефтегаз Ресчетка/Local grid refinement.py
+++ Нефтегаз Ресчетка/Local grid refinement.py
@@ -1,29 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def create_refined_grid(x_min, x_max, y_min, y_max, x_refine, y_refine, refine_radius, coarse_step, fine_step):
+def create_refined_grid(x_grid, y_grid, x_refine, y_refine, refine_radius, fine_step):
     # Создаем грубую сетку
-    x_coarse = np.arange(x_min, x_max, coarse_step)
-    y_coarse = np.arange(y_min, y_max, coarse_step)
-    X_coarse, Y_coarse = np.meshgrid(x_coarse, y_coarse)
+    X_coarse, Y_coarse = np.meshgrid(x_grid, y_grid)
 
     # Создаем уточненную сетку вокруг точки (x_refine, y_refine)
     x_fine = np.arange(x_refine - refine_radius, x_refine + refine_radius, fine_step)
     y_fine = np.arange(y_refine - refine_radius, y_refine + refine_radius, fine_step)
     X_fine, Y_fine = np.meshgrid(x_fine, y_fine)
 
-    # Добавляем нулевые строки к меньшему массиву, чтобы их размеры совпадали
-    if X_coarse.shape[0] > X_fine.shape[0]:
-        X_fine = np.vstack([X_fine, np.zeros((X_coarse.shape[0] - X_fine.shape[0], X_fine.shape[1]))])
-        Y_fine = np.vstack([Y_fine, np.zeros((Y_coarse.shape[0] - Y_fine.shape[0], Y_fine.shape[1]))])
-    elif X_coarse.shape[0] < X_fine.shape[0]:
-        X_coarse = np.vstack([X_coarse, np.zeros((X_fine.shape[0] - X_coarse.shape[0], X_coarse.shape[1]))])
-        Y_coarse = np.vstack([Y_coarse, np.zeros((Y_fine.shape[0] - Y_coarse.shape[0], Y_coarse.shape[1]))])
-
     # Объединяем грубую и уточненную сетки
-    X = np.concatenate((X_coarse, X_fine), axis=1)
-    Y = np.concatenate((Y_coarse, Y_fine), axis=1)
+    X = np.concatenate((X_coarse.flatten(), X_fine.flatten()))
+    Y = np.concatenate((Y_coarse.flatten(), Y_fine.flatten()))
 
     # Удаляем дубликаты и сортируем
     X, Y = np.unique(X), np.unique(Y)
@@ -31,19 +20,18 @@ def create_refined_grid(x_min, x_max, y_min, y_max, x_refine, y_refine, refine_r
     # Создаем полную сетку
     X_full, Y_full = np.meshgrid(X, Y)
 
+
     return X_full, Y_full
 
-
 # Параметры сетки
-x_min, x_max = 0, 10
-y_min, y_max = 0, 10
+x_grid = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])  # Неравномерная сетка по X
+y_grid = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])  # Неравномерная сетка по Y
 x_refine, y_refine = 5, 5  # Точка, вокруг которой будет уточнение
 refine_radius = 2  # Радиус уточнения
-coarse_step = 1  # Шаг грубой сетки
 fine_step = 0.1  # Шаг уточненной сетки
 
 # Создаем сетку с уточнением
-X, Y = create_refined_grid(x_min, x_max, y_min, y_max, x_refine, y_refine, refine_radius, coarse_step, fine_step)
+X, Y = create_refined_grid(x_grid, y_grid, x_refine, y_refine, refine_radius, fine_step)
 
 # Визуализация сетки
 plt.scatter(X, Y, s=1)
