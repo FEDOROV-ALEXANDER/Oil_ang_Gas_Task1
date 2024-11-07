bbf87fa2c6bf2165dce4436f986b975cfee8e96e
FEDOROV <fedorov.alexander.04@gmail.com>
2024-11-07 10:58:24 +0300

    На данный момент считает не правильно, очень странные значения давлений на забоях и не распределяется давление на площади

--- Нефтегаз Ресчетка/Solve.py
+++ Нефтегаз Ресчетка/Solve.py
@@ -9,28 +9,35 @@ def well_boundary_condition(X, Y, p, q, coef, N_x, N_y, r_w):
         for j in range(N_y):
             r = np.sqrt((X[i]) ** 2 + (Y[j]) ** 2)
             if r <= r_w:
-                p[i, j] += 13.76
+                p[i, j] += 10 # - q * coef * r_w * r_w / 2
                 # print(p[i, j])# q * coef * r_w
     return p
 
 
 def solve_for_one_well_implicit(X, Y, x_w, y_w, q, r_w, coef, pressure_start, T, eta):
     begin = tm.time()
-    pressure_w = []
+    pressure_w = [0]
     X = X - x_w
     Y = Y - y_w
     dx = X[1] - X[0]
     dy = Y[1] - Y[0]
     N_x, N_y = X.shape[0], Y.shape[0]
+
     dt = 1  # шаг по времени в сутках
-    time = []
+
+    dt = 1 / (2 * eta * (dx ** 2 + dy ** 2)) * dx ** 2 * dy ** 2  # шаг по времени в сутках c учетом устойчивости
+    if dt > T: dt = T // 100
+
+
+    time = [0]
     a, b = np.where(X == 0), np.where(Y == 0)
 
     # Коэффициенты для системы уравнений
     alpha = eta * dt / (dx ** 2)
     beta = eta * dt / (dy ** 2)
 
-    for t in range(1, T + 1, dt):
+    for t in range(0, T + 1, dt):
+        if t == 0: pass
         time.append(t)
 
         # Создание матрицы и вектора правой части
@@ -46,34 +53,33 @@ def solve_for_one_well_implicit(X, Y, x_w, y_w, q, r_w, coef, pressure_start, T,
         # Граничное условие на скважине
         pressure_start = well_boundary_condition(X, Y, pressure_start, q, coef, N_x, N_y, r_w)
         pressure_w.append(pressure_start[int(a[0]), int(b[0])])
-        print(t, tm.time() - begin)
+        print(t, tm.time() - begin, pressure_start[int(a[0]), int(b[0])])
     return pressure_start, pressure_w, time
 
 
 def solve_for_one_well_explicit(X, Y, x_w, y_w, q, r_w, coef, pressure_start, T, eta):
-    pressure_w = []
+    pressure_w = [0]
     X = X - x_w
     Y = Y - y_w
     dx = X[1] - X[0]
     dy = Y[1] - Y[0]
     N_x, N_y = X.shape[0], Y.shape[0]
     dt = 1 / (2 * eta * (dx ** 2 + dy ** 2)) * dx ** 2 * dy ** 2  # шаг по времени в сутках c учетом устойчивости
-    if dt > 5: dt = 5
-    time = []
+    if dt > T: dt = T // 100
+    time = [0]
     a, b = np.where(X == 0), np.where(Y == 0)
 
-    for t in range(1, T + 1, dt):
+    for t in range(0, T + 1, dt):
+        if t == 0: pass
         time.append(t)
 
         p_new = pressure_start.copy()
-        for i in range(1, N_x - 1):
-            for j in range(1, N_y - 1):
-                p_new[i, j] = pressure_start[i, j] + eta * dt * (
-                        (pressure_start[i + 1, j] - 2 * pressure_start[i, j] + pressure_start[i - 1, j]) / dx ** 2 +
-                        (pressure_start[i, j + 1] - 2 * pressure_start[i, j] + pressure_start[i, j - 1]) / dy ** 2
-                )
+        # Векторизованное обновление давления
+        p_new[1:-1, 1:-1] = pressure_start[1:-1, 1:-1] + eta * dt * (
+                (pressure_start[2:, 1:-1] - 2 * pressure_start[1:-1, 1:-1] + pressure_start[:-2, 1:-1]) / dx ** 2 +
+                (pressure_start[1:-1, 2:] - 2 * pressure_start[1:-1, 1:-1] + pressure_start[1:-1, :-2]) / dy ** 2
+        )
         pressure_start = well_boundary_condition(X, Y, p_new, q, coef, N_x, N_y, r_w)
-        print(p_new[int(a[0]), int(b[0])], int(a[0]), int(b[0]), pressure_start[int(a[0]), int(b[0])])
         pressure_w.append(pressure_start[int(a[0]), int(b[0])])
 
     return pressure_start, pressure_w, time
--- Нефтегаз Ресчетка/main.py
+++ Нефтегаз Ресчетка/main.py
@@ -19,21 +19,24 @@ def choose_step(length, width, x_wells, y_wells):
     return dx, dy
 
 # координаты скважины
-well_1 = Well(550, 850, 1.5, 10)
-well_2 = Well(10, 90, 1.5, 10)
-well_3 = Well(32, 16, 1.5, -10)
-well_4 = Well(45, 90, 1.5, -10)
+well_1 = Well(10, 20, 1.5, 10)
+well_2 = Well(10, 20, 1.5, 10)
+well_3 = Well(10, 20, 1.5, 10)
+well_4 = Well(10, 20, 1.5, 10)
+well_1 = Well(10, 20, 1.5, 10)# well_2 = Well(10, 90, 1.5, 10)
+# well_3 = Well(32, 16, 1.5, -10)
+# well_4 = Well(45, 90, 1.5, -10)
 
 # пока будет одна скважина, нагнетательная
 
 # Ввод входных значений
-length, width = 1000, 1000  # [м] геометрические размеры рассчитываемой области
+length, width = 500, 500  # [м] геометрические размеры рассчитываемой области
 dx, dy = choose_step(length, width, [well_1.x_w, well_2.x_w, well_3.x_w, well_4.x_w],
                      [well_1.y_w, well_2.y_w, well_3.y_w, well_4.y_w])   # [м] шаг по направлениям
 Nx, Ny = int(length / dx) + 1, int(width / dy) + 1  # количество элементов
 X = np.linspace(0, length, Nx)
 Y = np.linspace(0, width, Ny)
-T = 10  # время в сутках
+T = 1000  # время в сутках
 
 B = 1.2  # Объемный коэффициент
 h = 10  # толщина пласта (м)
@@ -60,7 +63,7 @@ pressure_w = np.array(pressure_w)
 print(t.shape, pressure_w.shape)
 
 # Визуализация распределения давления
-plt.figure()
+plt.figure(figsize=(8, 6))
 plt.pcolormesh(X, Y, pressure, shading='nearest', cmap='viridis')
 plt.colorbar(label='Давление')
 plt.title(f'Распределение давления через {T} дней')
@@ -77,24 +80,24 @@ plt.ylabel('Давление на забое скважины')
 plt.show()
 
 # Используем явный метод
-# pressure, pressure_w, t = solve_for_one_well_explicit(X.copy(), Y.copy(), well_1.x_w, well_1.y_w, well_1.q, well_1.r_w, coef, pressure_start.copy(), T, eta)
-# t = np.array(t)
-# pressure_w = np.array(pressure_w)
-# print(t.shape, pressure_w.shape)
-#
-# # Визуализация распределения давления
-# plt.figure(figsize=(8, 6))
-# plt.pcolormesh(X, Y, pressure, shading='auto', cmap='viridis')
-# plt.colorbar(label='Давление')
-# plt.title(f'Распределение давления через {T} дней')
-# plt.xlabel('X')
-# plt.ylabel('Y')
-# plt.title('Явный')
-# plt.show()
-#
-# plt.figure(figsize=(8, 6))
-# plt.plot(t, pressure_w)
-# plt.xlabel('Время (дни)')
-# plt.title('Явный')
-# plt.ylabel('Давление на забое скважины')
-# plt.show()
+pressure, pressure_w, t = solve_for_one_well_explicit(X.copy(), Y.copy(), well_1.x_w, well_1.y_w, well_1.q, well_1.r_w, coef, pressure_start.copy(), T, eta)
+t = np.array(t)
+pressure_w = np.array(pressure_w)
+print(t.shape, pressure_w.shape)
+
+# Визуализация распределения давления
+plt.figure(figsize=(8, 6))
+plt.pcolormesh(X, Y, pressure, shading='auto', cmap='viridis')
+plt.colorbar(label='Давление')
+plt.title(f'Распределение давления через {T} дней')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Явный')
+plt.show()
+
+plt.figure(figsize=(8, 6))
+plt.plot(t, pressure_w)
+plt.xlabel('Время (дни)')
+plt.title('Явный')
+plt.ylabel('Давление на забое скважины')
+plt.show()
